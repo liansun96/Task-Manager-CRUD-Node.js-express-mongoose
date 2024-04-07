e35844811951001e552d1f89f3171674bebd68bf
liansun96 <toetetaung3296@gmail.com>
2024-04-07 22:18:00 +0630

    custom erro api

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -5,6 +5,8 @@ const { createCustomError } = require('../errors/custom-error')
 const getAllTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find({});
   res.status(200).json({ tasks });
+  // res.status(200).json({ tasks  , amount : tasks.length});
+  // res.status(200).json({ status : "success" , data : {tasks , nbHits : tasks.length}});
 });
 
 const createTask = asyncWrapper(async (req, res) => {
